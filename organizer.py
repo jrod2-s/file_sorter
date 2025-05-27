c33d5997c9b4f8ce6a44d6c346a8ddc73bd72f14
Juan Rodriguez <juan.rodriguez.2928@gmail.com>
2025-05-26 20:03:58 -0700

    init: Initial commit

---
+++ organizer.py
@@ -0,0 +1,64 @@
+import os
+from abc import ABC, abstractmethod
+
+from flask import Flask, request, send_file, render_template
+from PIL import Image
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+CONVERTED_FOLDER = 'converted'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(CONVERTED_FOLDER, exist_ok=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/organize', methods=['POST'])
+def organize():
+    #TODO: 
+    # Create a function that goes through all the files
+    # If it is a JPEG, PNG, TIFF, RAW, MP4, MOV, etc, obtain internal timestamp
+    # If it is any other file, get the timestamp given to it by the filesystem
+    if 'image' not in request.files:
+        return "No file part", 400
+    file = request.files['image']
+    if file.filename == '':
+        return "No selected file", 400
+    if file and file.filename.lower().endswith(('.jpg', '.jpeg')):
+        filename = secure_filename(file.filename)
+        jpg_path = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(jpg_path)
+
+        # Convert to PNG
+        img = Image.open(jpg_path)
+        png_filename = filename.rsplit('.', 1)[0] + '.png'
+        png_path = os.path.join(CONVERTED_FOLDER, png_filename)
+        img.save(png_path, 'PNG')
+
+        return send_file(png_path, as_attachment=True)
+
+    return "Invalid file type. Please upload a JPEG.", 400
+
+class FileTimeStamp(ABC):
+    """ Abstract Class to obtain timestamp. """
+    pass
+
+class JPGTimeStamp(FileTimeStamp):
+    """ Class to obtain timestamp for the JPG. """
+    pass
+
+class PNGTimeStamp(FileTimeStamp):
+    """ Class to obtain timestamp for the PNG. """
+    pass
+
+class MP4TimeStamp(FileTimeStamp):
+    """ Class to obtain timestamp for the MP4. """
+    pass
+
+#TODO: Include other filetypes and fill in logic
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
