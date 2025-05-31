5b907ee4b8bb1d990ec162254b83adc28d98ffa5
Juan Rodriguez <juan.rodriguez.2982@gmail.com>
2025-05-31 08:49:30 -0700

    feat: Include code to parse mp4, update parsing to use strptime

--- organizer.py
+++ organizer.py
@@ -1,11 +1,10 @@
 import os
-from abc import ABC, abstractmethod
-import re
 
 from datetime import datetime
 from flask import Flask, request, send_file, render_template
 from PIL import Image
 from werkzeug.utils import secure_filename
+from pymediainfo import MediaInfo
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
@@ -48,18 +47,15 @@ def jpg_time_stamp(file):
     image = Image.open(file)
     exif_data = image._getexif()
 
-    try:
-        time_list = re.split(r'[:,\s]+', exif_data[306])
-
-        date = datetime(int(time_list[0]), int(time_list[1]),int(time_list[2]), int(time_list[3]), int(time_list[4]), int(time_list[5]))
-
-        file_time = date.strftime("%Y_%m_%d_%H_%M_%S")
+    if 306 in exif_data:
+        datetime_obj = datetime.strptime(exif_data[306], "%Y:%m:%d %H:%M:%S")
+        file_time = datetime_obj.strftime("%Y_%m_%d_%H_%M_%S")
 
         file_name = file_time + "_" + file
 
         return file_name
     
-    except:
+    else:
         # Return None if there is no EXIF data
         return None
 
@@ -68,25 +64,53 @@ def png_time_stamp(file):
 
     image = Image.open(file)
 
-    if 'Creation Time' in image.info:
-        date = image.info['Creation Time']
+    if "Creation Time" in image.info:
+        exif_data = image.info['Creation Time']
     elif "timestamp" in image.info:
-        date = image.info['timestamp']
+        exif_data = image.info['timestamp']
+    elif "data" in image.info:
+        exif_data = image.info['date']
     else:
         return None
     
-    return date
+    datetime_obj = datetime.strptime(exif_data, "%Y:%m:%d %H:%M:%S")
+    file_time = datetime_obj.strftime("%Y_%m_%d_%H_%M_%S")
+
+    file_name = file_time + "_" + file
+
+    return file_name
 
 def mp4_time_stamp(file):
     """ Class to obtain timestamp for the MP4. """
+    media_info = MediaInfo.parse(file)
 
-    pass
+    for track in media_info.tracks:
+        if track.track_type == "General":
+            time_metadata = track.encoded_date
+
+            datetime_obj = datetime.strptime(time_metadata, "%Y-%m-%d %H:%M:%S %Z")
+            
+            file_time = datetime_obj.strftime("%Y_%m_%d_%H_%M_%S")
+
+            file_name = file_time + "_" + file
+
+            return file_name
+
+        else:
+            return None
+        
+
+
+
+    
 
 #TODO: Include other filetypes and fill in logic
 
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    # print(jpg_time_stamp('scar.png'))
+    print(jpg_time_stamp('bros.jpg'))
 
     print(png_time_stamp("scar.png"))
+
+    print(mp4_time_stamp("rocket.mp4"))
