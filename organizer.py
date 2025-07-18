233fd6af36df5c36464550bd46675994f7e509cc
Juan Rodriguez <juan.rodriguez.2982@gmail.com>
2025-07-18 12:48:13 -0700

    feat: working timestamp retrieval

--- organizer.py
+++ organizer.py
@@ -6,7 +6,8 @@ from werkzeug.utils import secure_filename
 from timestamp import *
 from utilities import *
 
-#TODO: Deploy this and figure out what else is needed along the way
+#TODO: Figure out why time stamp is not being saved when uploading ...
+# The array return nothing, reference test code
 #TODO: Test website to see if it works with different time zones (make compatible with different timezones)
 #TODO: Remove personal email and add email message submission
 
@@ -38,104 +39,86 @@ def organize():
     # Make upload folder
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     # clean up random zip files
-    delete_zip()
+    # delete_zip()
     
     # Obtain zip file from user
-    file = request.files['file']
-    zipfile = secure_filename(file.filename)
+    files = request.files.getlist('files')
+    metadata = request.form.getlist('metadata')
+    print("Flask")
+    print(metadata)
 
-    # Check if file was inputted
-    if zipfile == "":
-        #TODO: Make simple text response not allowing you to not enter a file
-        abort(404)
-
-    # define booleans to note each file type
-    zip = zipfile.lower().endswith((".zip"))
-    seven_z = zipfile.lower().endswith((".7z"))
-
-    if zip:
-        parse_val = -4
-    elif seven_z:
-        parse_val = -3
-
-
-    # Save off the zip file in the uploads folder
-    if zip or seven_z:
-        zip_path = os.path.join(UPLOAD_FOLDER, zipfile).replace(os.sep, "/")
-        file.save(zip_path)
-    else:
-        raise Exception("Files extension is missing or not supported.")
-
-
-    # Extract the zip file
-    extracted_folder = zipfile[0:parse_val] + "_extracted"
-    extracted_path = os.path.join(UPLOAD_FOLDER, extracted_folder).replace(os.sep, "/")
-
-    if zip:
-        secure_extract(zip_path, extracted_path)
-    elif seven_z:
-        secure_extract_7z(zip_path, extracted_path)
-
-    # Obtain a list of all the files within the folder
-    extracted_subfolder = zipfile[0:parse_val]
-
-    subfolder_path = os.path.join(UPLOAD_FOLDER, extracted_folder, extracted_subfolder).replace(os.sep, "/")
-
-    files = [f for f in os.listdir(subfolder_path) if os.path.isfile(os.path.join(subfolder_path, f))]
-
-    # Loop through the list and change the name of the file in that extracted folder
-    paths = []
 
     for file in files:
-        file_name = file
-        file_path = os.path.join(subfolder_path, file).replace(os.sep, "/")
+        file_path = file.filename
+        file_path = os.path.normpath(file_path)
+
+
+        return "done"
+
+        parts = file_path.split(os.path.sep)
+        safe_parts = [secure_filename(part) for part in parts]
+
+        # Recombine into a safe relative path
+        safe_path = os.path.join(*safe_parts)
+
+        full_path = os.path.join(UPLOAD_FOLDER, safe_path)
+        file_name = os.path.basename(full_path)
+
+        os.makedirs(os.path.dirname(full_path), exist_ok=True)
+
+        file.save(full_path)
+
+        paths = []
 
         # Set file name output value to none
         date_filename = None
 
        # Check if file already has timestamp
         if already_time_stamped(file_name):
-            paths.append(file_path)
+            paths.append(full_path)
             continue
 
         # Timestamp a jpg
         elif file_name.lower().endswith(('.jpg', '.jpeg')): 
-            date_filename = jpg_time_stamp(file_path)
+            date_filename = jpg_time_stamp(full_path)
         
         # Timestamp a png
         elif file_name.lower().endswith(('.png')):
-            date_filename = png_time_stamp(file_path)
+            date_filename = png_time_stamp(full_path)
 
         # Timestamp a mp4
         elif file_name.lower().endswith(('.mp4')):
-            date_filename = mp4_time_stamp(file_path)
+            date_filename = mp4_time_stamp(full_path)
 
         # Timestamp other files with filesystem time
         else: 
-            date_filename = filesystem_time_stamp(file_path)
+            date_filename = from_timestamp(full_path, timestamp)
         
         # If any of the special files cannot locate a timestamp, use filesystem time
         if date_filename is None:
-            date_filename = filesystem_time_stamp(file_path)
+            date_filename = from_timestamp(full_path, timestamp)
+
+        path = os.path.dirname(full_path)
 
         # change file path name
-        stamped_path = os.path.join(extracted_path, date_filename).replace(os.sep, "/")
+        stamped_path = os.path.join(path, date_filename).replace(os.sep, "/")
+        print(stamped_path)
         
         # Rename the file in extracted subfolder to new name
         stamped_path = unique_filename(stamped_path)
-        os.rename(file_path, stamped_path)
+        os.rename(full_path, stamped_path)
 
         paths.append(stamped_path)
 
-    # Zip up the files
-    stamped_file_name = f"stamped_{zipfile}"
+    # # Zip up the files
+    # stamped_file_name = f"stamped_{zipfile}"
 
-    with ZipFile(stamped_file_name, "w") as zip:
-        for file in paths:
-            zip.write(file)
+    # with ZipFile(stamped_file_name, "w") as zip:
+    #     for file in paths:
+    #         zip.write(file)
 
-    # Delete the uploads folder
-    delete_uploads(UPLOAD_FOLDER)
+    # # Delete the uploads folder
+    # delete_uploads(UPLOAD_FOLDER)
 
     return send_file(stamped_file_name, as_attachment=True)
 
@@ -147,4 +130,4 @@ def organize():
     # If it is any other file, get the timestamp given to it by the filesystem
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
